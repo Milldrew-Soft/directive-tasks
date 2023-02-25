f6b532ae0216b32f089dd529ba9510c37972d99b
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-02-25 15:54:03 -0700

    fixed varible naming

--- bin/start-task.sh
+++ bin/start-task.sh
@@ -1,5 +1,5 @@
 task_suffix=$1
 new_branch_name=task"$task_suffix"•$(date +"%Y-%m-%d•hour%H")
-git checkout task"$task_number" && \
+git checkout task"$task_suffix" && \
 git checkout -b $new_branch_name && \
 less README.txt
